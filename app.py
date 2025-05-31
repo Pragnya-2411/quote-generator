23f3ca972c21c469afdf2e7af92b8e43f0a7bb80
Pragnya <panda.saipragnya@gmail.com>
2025-05-31 20:11:10 +0530

    Add styling, HTML and app code improvements.

--- app.py
+++ app.py
@@ -3,18 +3,16 @@ import requests
 
 app = Flask(__name__)
 
-def get_quote():
+@app.route("/")
+def home():
     response = requests.get("https://zenquotes.io/api/random")
     if response.status_code == 200:
         data = response.json()
-        return f'{data[0]["q"]} — {data[0]["a"]}'
+        quote = data[0].get("q")  # use "q" for the quote
+        author = data[0].get("a")  # use "a" for the author
+        return render_template("index.html", quote=quote, author=author)
     else:
-        return "Could not fetch a quote at the moment."
-
-@app.route("/")
-def home():
-    quote = get_quote()
-    return render_template("index.html", quote=quote)
+         return "Could not fetch a quote at the moment."
 
 if __name__ == "__main__":
     app.run(debug=True)
